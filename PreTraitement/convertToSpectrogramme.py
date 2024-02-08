9420f6db16fd029ee2809aa111faefd80e163c92
djo2000 <geoffrey.brenne@gmail.com>
2024-02-08 15:39:59 +0100

    modified:   Notebooks/Analyse_DataSet/statistique.ipynb 	modified:   PreTraitement/convertToSpectrogramme.py

--- PreTraitement/convertToSpectrogramme.py
+++ PreTraitement/convertToSpectrogramme.py
@@ -4,21 +4,34 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
+
+def scale_minmax(X, min=0.0, max=1.0):
+    X_std = (X - X.min()) / (X.max() - X.min())
+    X_scaled = X_std * (max - min) + min
+    return X_scaled
 
 def convert_wav_to_spectrogram(wav_path, save_path):
     # Charger l'audio
     y, sr = librosa.load(wav_path, sr=None)
     # Calculer le spectrogramme
     D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+
+    img = scale_minmax(D, 0, 255).astype(np.uint8)
+    img = np.flip(img, axis=0)
+
+    im = Image.fromarray(img)
+    im.save(save_path)
+
     
-    # Créer la figure sans les axes
-    fig, ax = plt.subplots(figsize=(10, 4))
+    """# Créer la figure sans les axes
+    fig, ax = plt.subplots()
     im = librosa.display.specshow(D, sr=sr, x_axis=None, y_axis=None, ax=ax)
-    plt.axis('off')  # Désactiver les axes
+    print(type(im))
     
     # Sauvegarder le spectrogramme sans les axes comme image
     plt.savefig(save_path, bbox_inches='tight', pad_inches=0)
-    plt.close()  # Fermer la figure pour libérer la mémoire
+    plt.close()  # Fermer la figure pour libérer la mémoire"""
 
 def convert_folder(input_folder, output_folder):
     # S'assurer que le dossier de sortie existe
@@ -41,9 +54,10 @@ def convert_folder(input_folder, output_folder):
     print("Conversion completed.")
 
 # Remplacez ces chemins par vos chemins de dossiers appropriés
-input_folder = "C:/Users/lucas/Documents/Annee_3_ENSEEIHT/Projet long/OrthopteraStridulationClassifier/Dataset acoustique insectes/Sélection morceaux audio 5s/Audible/val"
-output_folder = "C:/Users/lucas/Documents/Annee_3_ENSEEIHT/Projet long/OrthopteraStridulationClassifier/Dataset acoustique insectes/Sélection morceaux audio 5s/Audible/val_spectro"
+input_folder = "D:\OrthopteraStridulationClassifier\Dataset acoustique insectes\Sélection soundscapes 1 min"
+output_folder = "D:\OrthopteraStridulationClassifier\Dataset acoustique insectes\Sélection soundscapes 1min spectro"
 
-
+input_folder = "D:\OrthopteraStridulationClassifier\Dataset acoustique insectes\Sélection morceaux audio 5s\Audible\\train"
+output_folder = "D:\OrthopteraStridulationClassifier\Dataset acoustique insectes\Sélection morceaux audio 5s\Audible\\train_spectro"
 
 convert_folder(input_folder, output_folder)
